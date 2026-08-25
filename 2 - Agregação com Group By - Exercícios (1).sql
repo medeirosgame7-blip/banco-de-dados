8538584aaa1eec8c426f510b969816c970837aad
medeirosgame7-blip <medeirosgame7@gmail.com>
2026-08-25 16:39:41 -0300

    Add files via upload

---
+++ 2 - Agregação com Group By - Exercícios (1).sql
@@ -0,0 +1,23 @@
+/* LISTA DE EXERCÍCIOS - GROUP BY
+   Foco: Agrupamento de dados com COUNT, AVG, SUM, MAX
+*/
+
+-- 1. Filmes por Ano: Quantos filmes foram lançados em cada ano?
+
+-- 2. Duração Média por Gênero: Qual é a duração média (em minutos) dos filmes de cada gênero?
+
+-- 3. Atores por Nacionalidade: Quantos atores e atrizes temos cadastrados de cada nacionalidade?
+
+-- 4. Clientes por Estado: Quantos clientes temos em cada Estado brasileiro?
+
+-- 5. Receita Potencial por Gênero: Qual é a soma dos preços de aluguel de todos os filmes, agrupados por gênero?
+
+-- 6. Popularidade dos Filmes (Alugueis por Filme): Quantas vezes cada filme (pelo ID) foi alugado?
+
+-- 7. Comportamento do Cliente (Média de Notas): Qual é a nota média que cada cliente (pelo ID) costuma dar para os filmes?
+
+-- 8. Distribuição por Sexo (Atores): Quantos atores são do sexo masculino e quantos são do feminino?
+
+-- 9. Alugueis por Dia: Quantos alugueis foram realizados em cada data específica?
+
+-- 10. Maior Preço por Ano: Qual foi o filme mais caro (maior preço de aluguel) lançado em cada ano?
